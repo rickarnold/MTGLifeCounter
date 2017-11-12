af35f9f97f10c452a87c794c7ca8d4146629d521
rickarnold <rickthestickarnold@hotmail.com>
2017-11-12 16:22:23 -0700

    Update green backgroundColor

--- app/src/main/java/com/stick/mtglifecounter/Color.java
+++ app/src/main/java/com/stick/mtglifecounter/Color.java
@@ -4,7 +4,7 @@ package com.stick.mtglifecounter;
 public enum Color {
     BLACK("Black", 0xFF606060, 0xFF3b3b3b, R.drawable.black_symbol, R.raw.o_fortuna),
     BLUE("Blue", 0xFFa0d4f2, 0xFF0080a3, R.drawable.blue_symbol, R.raw.topgun),
-    GREEN("Green", 0xFF98e38a, 0xFF148200, R.drawable.green_symbol, R.raw.medieval_one),
+    GREEN("Green", 0xFF8dc290, 0xFF148200, R.drawable.green_symbol, R.raw.medieval_one),
     RED("Red", 0xFFff8080, 0xFF940000, R.drawable.red_symbol, R.raw.firestarter),
     WHITE("White", 0xFFf2f1bb, 0xFF9c9b76, R.drawable.white_symbol, R.raw.angel_of_death);
 
